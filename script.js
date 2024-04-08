6608ab66d803ed67e5495520349eb7b35e7aac5a
Jere1 <monserratjere@gmail.com>
2024-04-08 09:27:44 -0300

    actualizamos informacion, cambiamos el desplegable con x y mejoras, agregamos la libreria data-aos

--- script.js
+++ script.js
@@ -39,3 +39,13 @@ document.querySelector(".menu-toggle").addEventListener("click", function () {
   const menu = document.querySelector("header nav ul");
   menu.classList.toggle("menu-open");
 });
+
+//Cambia el menu por la x
+function menuX(){
+  var imagen = document.getElementById("imagen");
+  if(imagen.src.match("assets/x.png")){
+    imagen.src = "assets/desplegable.png"; // Cambia a la imagen desplegable
+  } else {
+    imagen.src = "assets/x.png"; // Cambia a la imagen de la X
+  }
+}
